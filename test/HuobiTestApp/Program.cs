7fca2311f24ec4191b409ca4352fb3c5cf02ff41
Alexey Novichikhin <alexey.n@smplt.net>
2022-08-28 00:33:51 +0300

    balance huobi

--- test/HuobiTestApp/Program.cs
+++ test/HuobiTestApp/Program.cs
@@ -3,6 +3,7 @@
 using Huobi.Net.Clients;
 using Huobi.Net.Enums;
 using MySettingsReader;
+using Newtonsoft.Json;
 using Service.Trading.Settings;
 
 
@@ -21,9 +22,23 @@ using (var client = new HuobiClient())
     client.SetApiCredentials(new CryptoExchange.Net.Authentication.ApiCredentials(settings.HuobiApiKey, settings.HuobiApiSecret));
     var accounts = await client.SpotApi.Account.GetAccountsAsync();
     if (accounts.Success) // Check the success flag for error handling
+    {
         Console.WriteLine($"Got account list, account id #1: {accounts.Data.First().Id}");
+    }
     else
         Console.WriteLine($"Failed to get account list, error: {accounts.Error}");
+
+    foreach (var acc in accounts.Data)
+    {
+        var balance = await client.SpotApi.Account.GetBalancesAsync(acc.Id);
+        Console.WriteLine($"Account {acc.Id}, Type: {acc.Type}");
+        foreach (var bal in balance.Data.Where(e => e.Balance > 0))
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(bal, Formatting.Indented));
+        }
+        Console.WriteLine("=====================\n");
+    }
+    
 }
 
 Console.WriteLine("");
